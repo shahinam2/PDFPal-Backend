f763c0f902013b8a7bbc7e79bf610787e0aaa147
shahin <shahin.ccie1989@gmail.com>
2023-01-03 12:01:03 +0100

    add comments to module 1

--- custom-modules/1.input-to-pdf.js
+++ custom-modules/1.input-to-pdf.js
@@ -4,29 +4,41 @@ const PDFDocument = require("pdfkit")
 const colors = require("colors");
 
 async function inputToPDF() {
+    // Create a readline interface
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
         raw: true,
     });
 
+    // Create a new PDF document
     const doc = new PDFDocument();
+    // Pipe the document to a file stream for saving
     doc.pipe(fs.createWriteStream('./output/output.pdf'));
 
+    // Initialize an empty string for storing inputted text
     let text = '';
 
+    // Set up an event listener for the 'line' event, which is triggered every time the user inputs a line of text
     rl.on('line', (line) => {
+        // Append the inputted line of text to the text string, with a newline character at the end
         text += line + '\n';
     });
 
+    // Set up an event listener for the 'SIGINT' event, which is triggered when the user presses 'CTRL + C' in the command line
     rl.on('SIGINT', () => {
+        // Close the readline interface
         rl.close();
     });
 
+    // Set up an event listener for the 'close' event, which is triggered when the readline interface is closed
     rl.on('close', () => {
+        // Add the text to the PDF document
         doc.text(text);
+        // End the PDF document and save it to a file
         doc.end();
 
+        // Print a green colored message to the console indicating that the PDF file has been created
         console.log('PDF file created: output.pdf'.green);
     });
 
